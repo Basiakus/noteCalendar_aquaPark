1afd71360e473ba496c8be3d491380a21754e096
basiakus <basiakus@gmail.com>
2019-02-10 15:44:45 +0100

    add div.breaker after each day in the main calendar to styling a weekend's days to the right place

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import DayMiniature from './DayMiniature';
 
 class Content extends Component {
 	render() {
+		// breacker is needed to push all days after sunday to a new row in the main calendar 
 		// Generate a array from number of days from state
 		let daysToArray = Array.from({length: this.props.state.date}, (v, k) => k); 
 		const {year, month} = this.props.state;
 		return (
 			<div className='content'>
 				{Object.keys(daysToArray).map( (day, i) => 
-
-					<DayMiniature 
-						id={`${day < 10 ? '0' + day : day}${month < 10 ? '0' + month : month}${year}`} 
-						key={day} index={day} 
-						{...this.props}
-					/> 
+					<Fragment key={`fragment${i}`}>
+						<DayMiniature 
+							id={`${day < 10 ? '0' + day : day}${month < 10 ? '0' + month : month}${year}`} 
+							key={day} index={day} 
+							{...this.props}
+						/>
+						<div className='breaker' key={`breaker${i}`}></div>
+					</Fragment>
 				)}
+
 			</div>
 		);
 	}
